32c04b94842bd096fbc1a837cb5dcaf410cf017d
Lucas <camposdlucasoli@gmail.com>
2024-08-25 10:24:10 -0300

    Chat passado para reactjs

---
+++ chatbot/src/App.jsx
@@ -0,0 +1,32 @@
+import {AppRouters} from "./router/index.jsx";
+import {BrowserRouter, useLocation} from "react-router-dom";
+import {useEffect} from "react";
+import {Helmet} from "react-helmet";
+
+function App() {
+    return (
+    <BrowserRouter>
+        <AppContent />
+    </BrowserRouter>
+  )
+}
+
+function AppContent(){
+    const getPageTitle = () => {
+        switch (location.pathname) {
+            case "/": return "ChatBot"
+            default: return "Error 404"
+        }
+    }
+
+    return (
+        <>
+            <Helmet>
+                <title>{getPageTitle()} | ChatBot</title>
+            </Helmet>
+            <AppRouters/>
+        </>
+    )
+}
+
+export default App
